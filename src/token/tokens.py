24b7a150ef66259298ed8e139a190db9ea034b24
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-16 16:00:12 -0300

    feat: Criação dos Tokes para utilização com o Parser.

---
+++ src/token/tokens.py
@@ -0,0 +1,34 @@
+from enum import Enum
+from dataclasses import dataclass
+from typing import Any
+
+
+class TokenType(Enum):
+    """
+    Type of tokens
+    """
+
+    NUMBER = 0
+    PLUS = 1
+    MINUS = 2
+    MULTIPLY = 3
+    DIVIDE = 4
+    POWER = 5
+    LOG = 6
+    LEFT_PARENTHESES = 7
+    RIGHT_PARENTHESES = 8
+
+
+@dataclass
+class Token:
+    """
+    Minimal package of information to use in Lexer
+    """
+
+    type: TokenType
+    value: Any = None
+
+    def __repr__(self) -> str:
+        return (
+            self.type.name + f": {self.value}" if self.value is not None else ": None"
+        )
