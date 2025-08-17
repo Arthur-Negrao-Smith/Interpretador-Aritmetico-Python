58677eff1469c5627d9fd872d714704a2e074322
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-16 22:10:50 -0300

    feat: Add a main.py to use the Lexer.

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from src.lexer.lexer import Lexer
+from typing import Generator
+
+import logging
+import sys
+
+if "debug" in sys.argv:
+    log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(
+        level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+msg: str = """
+============================
+        Calculator
+============================
+"""
+
+print(msg)
+while True:
+    text: str = input("Caculate >> ")
+    if text == "exit":
+        break
+    lexer: Lexer = Lexer(text)
+    tokens: Generator = lexer.generate_tokens()
+    print(list(tokens))
