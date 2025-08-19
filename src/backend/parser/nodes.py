7550cc1d8cc1f8ed68e44bd4ae33c80f75139698
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-19 13:08:23 -0300

    refactor: Change Nodes to be dataclass.

--- src/backend/parser/nodes.py
+++ src/backend/parser/nodes.py
@@ -1,4 +1,7 @@
 from typing import Literal
+from dataclasses import dataclass
+
+from backend.token.tokens import TokenType
 
 
 class Node:
@@ -10,127 +13,98 @@ class Node:
 
 
 # Literal nodes
+@dataclass
 class VariableNode(Node):
     """
     Node to use with variables
+
+    Attributes:
+        name (str): Name of variable
     """
-
-    def __init__(self, name: str) -> None:
-        """
-        Constructor from VariableNode
-
-        Args:
-            name (str): Name of variable
-        """
-        super().__init__()
-        self.name: str = name
+    name: str
 
     def __repr__(self) -> str:
         return f"{self.name}"
 
 
+@dataclass
 class NumberNode(Node):
     """
     Node to represent literal numbers
+
+    Attributes:
+    value (int): Literal value of the node
     """
-
-    def __init__(self, value: int | float) -> None:
-        """
-        Constructor from NumberNode
-
-        Args:
-            value (int): Literal value of the node
-        """
-        super().__init__()
-        self.value: int | float = value
+    value: int | float
 
     def __repr__(self) -> str:
         return f"{self.value}"
 
 
 # Operations
+@dataclass
 class BinOperationsNode(Node):
     """
     Binary node operations like: +, -, *, ^, ...
+
+    Attributes:
+        left_node (type[Node]): Node to use operation
+        operation (str): Operation to use with left and right node
+        right_node (type[Node]): Node to use operation
     """
-
-    def __init__(
-        self, operation: str, left_node: type[Node], right_node: type[Node]
-    ) -> None:
-        """
-        Contructor from BinOperationsNode
-
-        Args:
-            operation (str): Operation to use with left and right node
-            left_node (type[Node]): Node to use operation
-            right_node (type[Node]): Node to use operation
-        """
-        super().__init__()
-        self.operation: str = operation
-        self.left_node: type[Node] = left_node
-        self.right_node: type[Node] = right_node
+    left_node: Node
+    operation: str
+    right_node: Node
 
     def __repr__(self) -> str:
         return f"({self.left_node}{self.operation}{self.right_node})"
 
 
+@dataclass
 class UnaryOperationNode(Node):
     """
     Unary node to operations like: -x or +x
+
+    Attributes:
+        operation (Literal["+", "-"]): Operation to use with node
+        node (type[Node]): Node to use operator
     """
-
-    def __init__(self, operation: Literal["+", "-"], node: type[Node]) -> None:
-        """
-        Contructor from UnaryOperationNode
-
-        Args:
-            operation (str): Operation to use with node
-            node (type[Node]): Node to use operator
-        """
-        super().__init__()
-        self.operation: Literal["+", "-"] = operation
-        self.operand: type[Node] = node
+    operation: Literal["+", "-"]
+    operand: Node
 
     def __repr__(self) -> str:
         return f"({self.operand}{self.operation})"
 
 
 # Assignment
+@dataclass
 class AssignmentNode(Node):
     """
     Node to represent "="
+
+    Attributes:
+        variable_name (str): Name of variable
+        expression (Node): Node to represent the expression to assign
     """
-
-    def __init__(self, variable_name: str, expression: type[Node]) -> None:
-        """
-        Constructor from AsssignmentNode
-
-        Args:
-            variable_name (str): Name of variable
-            expression (type[Node]): Node to represent the expression to assign
-        """
-        super().__init__()
-        self.variable_name: str = variable_name
-        self.value: type[Node] = expression
+    variable_name: str
+    value: Node
 
     def __repr__(self) -> str:
         return f"{self.variable_name}={self.value}"
 
 
 # Functions Node
+@dataclass
 class FunctionNode(Node):
     """
     Node to represent functions like: sqrt, log, sin, cos, ...
+
+    Attributes:
+        function_type (TokenType): TokenType of used function
+        expression (Node): Expression to use in function args
     """
+    function_type: TokenType
+    expression: Node
 
-    def __init__(self, function_name: str, expression: type[Node]) -> None:
-        """
-        Constructor from FunctionNode
-
-        Args:
-            function_name (str): Name of used function
-            expression (type[Node]): Expression to use in function args
-        """
-        super().__init__()
-        self.function_name: str = function_name
-        self.args: type[Node] = expression
+    def __repr__(self) -> str:
+        return f"{self.function_type}({self.expression})"
