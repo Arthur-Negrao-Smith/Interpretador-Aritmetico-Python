29607f0f41cb22c85fead27956c975dfa9c94c09
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-19 13:40:21 -0300

    refactor: Create the blue print of the Parse.

--- src/backend/parser/arithmetic_parser.py
+++ src/backend/parser/arithmetic_parser.py
@@ -1,8 +1,8 @@
+from types import FunctionType
 from src.backend.parser.nodes import *
-from src.backend.token.tokens import Token
+from src.backend.token.tokens import Token, TokenType
 
 from typing import Generator
-
 
 class Parser:
     """
@@ -49,10 +49,67 @@ class Parser:
         except StopIteration:
             self.current_token = None
 
-    def nud(self) -> Token:
+    @staticmethod
+    def left_bind_power(token: Token) -> int:
+        """
+        Function to return bind power of the operator
+
+        Args:
+            token (Token): Token to return the bind power
+
+        Returns:
+            int: Bind power fo the operation
+
+        Raises:
+            RuntimeError: If the Token not in simple operations
+        """
+        match token.type:
+            case TokenType.EQUAL:
+                return 1
+            case TokenType.PLUS:
+                return 10
+            case TokenType.MINUS:
+                return 10
+            case TokenType.MULTIPLY:
+                return 20
+            case TokenType.DIVIDE:
+                return 20
+            case TokenType.POWER:
+                return 30
+            case _:
+                raise RuntimeError(f"Bad token: '{token}'")
+
+
+    def nud(self, token: Token) -> Node:
+        """
+        Parse Literal values and Function calls
+
+        Args:
+            token (Token): Token to parse and create Node
+        """
+        if token.type == TokenType.NUMBER:
+            return NumberNode(token.value)
+
+        if token.type in (TokenType.VARIABLE, TokenType.COS, TokenType.LOG, TokenType.EXP, TokenType.SIN):
+
+            if token.type == TokenType.LEFT_PARENTHESES:
+                pass
+
+
+
+        raise SyntaxError(f"Bad token: '{token}'")
+
+    def led(self, token: Token, left_node: Node) -> Token:
+        """
+        Parse Operations like: +, -, /, *, ^ and =
+
+        Args:
+            token (Token): Token to parse and create Node
+            left_node (Node): Left node in syntax tree
+        """
         pass
 
-    def led(self) -> Token:
+    def expression(self) -> Node:
         pass
 
     def parse(self) -> None:
