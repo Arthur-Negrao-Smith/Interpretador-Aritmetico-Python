10831fd053a705056f7ecd78a223a2a53ca1bb20
Itallo Cavalcante <itallocavalcante07@gmail.com>
2025-08-19 23:21:25 -0300

    Feat: Suporte para operação unária no interpretador

--- src/backend/interpreter/interpreter.py
+++ src/backend/interpreter/interpreter.py
@@ -34,3 +34,39 @@ class Interpreter:
         else:
             raise Exception(f"Operação desconhecida: {operation_}")
         
+from src.backend.parser import *
+from values import Number
+
+class Interpreter: 
+    def visit(self, node):
+        method_name = f'visit_{type(node).__name__}'
+        method = getattr(self, method_name)
+        return method(node)
+    
+    def visit_NumberNode(self, node):
+        return Number(node.value)
+
+    def visit_BinOperantionsNode(self, node):
+        left = self.visit(node.left_node)
+        right = self.visit(node.right_node)
+        operation_ = node.operation
+
+        if operation_ == '+':
+            return Number(left.Value + right.Value)
+        
+        if operation_ == '-':
+            return Number(left.Value - right.Value)
+        
+        if operation_ == '*':
+            return Number(left.Value * right.Value)
+        
+        if operation_ == '/':
+            if right.Value == 0:
+                raise ZeroDivisionError("Divisão por zero.")
+            return Number(left.Value / right.Value)
+        if operation_ == '^':
+            return Number(left.Value ** right.Value)
+        
+        else:
+            raise Exception(f"Operação desconhecida: {operation_}")
+        
