fd5106f7ee79bccf50de373e31eda87c1b15537d
Kamila Sarmanho <kamilasarmanho@gmail.com>
2025-08-21 10:33:06 -0300

    feat: Tratamento de resposta de API

--- src/ui/script.js
+++ src/ui/script.js
@@ -1,9 +1,50 @@
-let Total = 0;
+const result = document.getElementById('result');
+const inputField = document.getElementById('expression-input');
+const analyzeButton = document.getElementById('enter-btn');
+const errorArea = document.getElementById('error-area')
 
-const result = document.getElementById('.result');
+const apiUrl = a
 
 function addOperator(operator) {
-    const inputField = document.getElementById('expression-input');
     inputField.value += operator;
     inputField.focus()
 }
+
+const sendExpression = async () => {
+    const expression = inputField.value;
+    if (!expression) {
+        result.textContent = '0';
+        return;
+    }
+
+    result.textContent = '...';
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ expression: expression }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            result.textContent = `${data.result}`;
+        } else {
+            errorArea.textContent = data.type_error, data.error;
+        }
+    } catch (error) {
+        console.error('Erro ao conectar com a API:', error);
+        errorArea.textContent = 'Não foi possível conectar ao servidor.';
+    }
+};
+
+analyzeButton.addEventListener('click', sendExpression());
+
+inputField.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        sendExpression();
+    }
+});
