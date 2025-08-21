38ac510fa7a489122ef43f492fe4c8901c00904c
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-20 22:57:56 -0300

    docs: Add typehints to interpreter.py.

--- src/backend/interpreter/interpreter.py
+++ src/backend/interpreter/interpreter.py
@@ -1,24 +1,30 @@
-from src.backend.parser import *
 from src.backend.interpreter.values import Number
+from src.backend.parser.nodes import *
+
+from typing import Callable
+
 import math
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class Interpreter:
     def __init__(self):
-        self.variables = {}
+        self.variables: dict[str, Number] = {}
 
-    def visit(self, node):
-        method_name = f"visit_{type(node).__name__}"
-        method = getattr(self, method_name)
+    def visit(self, node) -> Number:
+        method_name: str = f"visit_{type(node).__name__}"
+        method: Callable = getattr(self, method_name)
         return method(node)
 
-    def visit_NumberNode(self, node):
+    def visit_NumberNode(self, node: NumberNode) -> Number:
         return Number(node.value)
 
-    def visit_BinOperationNode(self, node):
-        left = self.visit(node.left_node)
-        right = self.visit(node.right_node)
-        operation_ = node.operation
+    def visit_BinOperationNode(self, node: BinOperationNode) -> Number:
+        left: Number = self.visit(node.left_node)
+        right: Number = self.visit(node.right_node)
+        operation_: str = node.operation
 
         if operation_ == "+":
             return Number(left.Value + right.Value)
@@ -31,26 +37,27 @@ class Interpreter:
 
         elif operation_ == "/":
             if right.Value == 0:
-                raise ZeroDivisionError("Divisão por zero.")
+                raise ZeroDivisionError("Division by zero is not allowed.")
             return Number(left.Value / right.Value)
+
         elif operation_ == "^":
             return Number(left.Value**right.Value)
 
         else:
-            raise Exception(f"Operação desconhecida: {operation_}")
+            raise RuntimeError(f"Unsuported operation: '{operation_}'.")
 
-    def visit_UnaryOperationNode(self, node):
-        value = self.visit(node.operand)
+    def visit_UnaryOperationNode(self, node: UnaryOperationNode) -> Number:
+        value: Number = self.visit(node.operand)
         if node.operation == "+":
             return Number(+value.Value)
         elif node.operation == "-":
             return Number(-value.Value)
-        else:
-            Exception(f"Operação desconhecida: {node.operaation}")
 
-    def visit_FunctionNode(self, node):
-        arg = self.visit(node.expression)
-        funct_name = node.function_name.lower()
+        raise RuntimeError(f"Unrecognized operation: '{node.operation}'.")
+
+    def visit_FunctionNode(self, node: FunctionNode) -> Number:
+        arg: Number = self.visit(node.expression)
+        funct_name: str = node.function_name.lower()
 
         if funct_name == "sqrt":
             return Number(math.sqrt(arg.Value))
@@ -62,16 +69,16 @@ class Interpreter:
             return Number(math.cos(arg.Value))
         elif funct_name == "exp":
             return Number(math.exp(arg.Value))
-        else:
-            raise Exception(f"Função desconhecida: {funct_name}")
 
-    def visit_AssignmentNode(self, node):
-        value = self.visit(node.value)
+        raise RuntimeError(f"Unsuported function: '{funct_name}'.")
+
+    def visit_AssignmentNode(self, node: AssignmentNode) -> Number:
+        value: Number = self.visit(node.value)
         self.variables[node.variable_name] = value
         return value
 
     def visit_VariableNode(self, node):
         if node.name in self.variables:
             return self.variables[node.name]
-        else:
-            raise Exception(f"Variável não definida: {node.name}")
+
+        raise ValueError(f"No value assigned to variable: '{node.name}'.")
